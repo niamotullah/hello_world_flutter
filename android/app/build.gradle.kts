cf565d92b700f9383ea650c156278684a941dee4
Niamot <58494481+niamotullah@users.noreply.github.com>
2025-06-29 02:56:53 +0600

    fix build dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,6 +9,8 @@ android {
     namespace = "com.example.hello_world"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
+    // ndkVersion = flutter.ndkVersion
+    ndkVersion =  "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -24,7 +26,8 @@ android {
         applicationId = "com.example.hello_world"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        // minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
